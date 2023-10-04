97fd6ad3245bf8af4926adaebbbd77b61d43c7fa
ARMILANCER <114140097+ARMILANCER@users.noreply.github.com>
2023-10-04 13:49:36 +0200

    Update Gestione.java

--- Gestione.java
+++ Gestione.java
@@ -1,19 +1,23 @@
 package Dizionario;
-
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Vector;
 
 public class Gestione {
+    private HashMap<String,Integer> memory = new HashMap<>();
+    private File file = new File("Dizionario.dat");
     private final int MAX_DIM_WORD = 26;
 
     public Gestione(){
         fillMemory();
     }
-    private HashMap<String,Integer> memory = new HashMap<>();
     public void addWord(String word){
-        try(RandomAccessFile raf = new RandomAccessFile("Dizionario.dat","rw")){
+        try(RandomAccessFile raf = new RandomAccessFile(file,"rw")){
+            raf.seek(raf.length());
             raf.writeBytes(word);
             for(int i=0;i<(MAX_DIM_WORD-word.length());i++){
                 raf.writeByte('\0');
@@ -23,14 +27,15 @@ public class Gestione {
         }
         memory.put(word,+1);
     }
-
     public void fillMemory(){
         int character = 0;
         StringBuilder word = new StringBuilder();
-        try(RandomAccessFile raf = new RandomAccessFile("Dizionario.dat","wr")){
+        try(RandomAccessFile raf = new RandomAccessFile(file,"r")){
             while((character = raf.read())!=-1){
-                for(int i=0;i<MAX_DIM_WORD;i++){
-                    word.append((char)character);
+                for(int i=0;i<MAX_DIM_WORD;i++) {
+                    if(character != '\0') {
+                        word.append((char) character);
+                    }
                 }
                 memory.put(word.toString(),+1);
             }
@@ -39,4 +44,12 @@ public class Gestione {
         }
     }
 
+    public String orderlyMemory(){
+        Set<Integer> treeSet = new TreeSet<>();
+        memory.forEach((key,value)->{
+            treeSet.add(value);
+        });
+
+        return "das";
+    }
 }
